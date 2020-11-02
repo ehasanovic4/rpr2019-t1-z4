7b6da55a07cf7ba63cdf668d689193830a94bb34
ehasanovic4 <73076960+ehasanovic4@users.noreply.github.com>
2020-11-02 11:06:23 +0100

    Update Artikl.java

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,38 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+  prtivate String naziv, kod;
+  private int cijena;
+  
+  public Artikl(String naziv, String kod, int cijena){
+    setNaziv(naziv);
+    setKod(kod);
+    setCijena(cijena);
+  }
+  
+  public String getNaziv(){
+    return naziv;
+  }
+  
+  public String getKod(){
+    return kod;
+  }
+  
+  public int getCijena(){
+    return cijena;
+  }
+  
+  public void setNaziv(String naziv){
+    this.naziv=naziv;
+  }
+  
+   public void setKod(String kod){
+    this.kod=kod;
+  }
+  
+   public void setCijena(int cijena){
+    this.cijena=cijena;
+  }
+  
+  
 }
