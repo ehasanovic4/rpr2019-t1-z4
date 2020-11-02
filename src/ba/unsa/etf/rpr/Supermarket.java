c80de7d1ada16c2f1dfbaf567551a196160a9801
ehasanovic4 <73076960+ehasanovic4@users.noreply.github.com>
2020-11-02 12:03:37 +0100

    Update Supermarket.java

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+  
+  private max = 1000;
+  private Artikl[] artikli;
+  private int brArtikala;
+  
+   public Supermarket(){
+    max=1000;
+    Artikl[] artikli = new Artikl[max];
+    brArtikala=0;    
+  }
+  
+  public boolean dodajArtikl(Artikl a){
+    if(brArtikala = max) return false;
+    else {
+      artikli[brArtikala] = a;
+      brArtikala++;
+      return true;
+    }    
+  }
+  
+  public Artikli[] getArtikl(){
+    return artikli;
+  }
+  
+  public Artikl izbaciArtiklSaKodom(String kod){
+    Artikl n = null;
+    for(int i=0;i<artikli.length;i++){
+      if(artikli[i]!=null && artikli[i].getKod().equals(kod)){
+        n=artikli[i];
+        artikli[i] = null;
+        break;
+      }
+    }
+    return n;
+  }
+  
+  
+  
+  
 }
