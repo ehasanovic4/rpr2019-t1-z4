7ee1b15ccab4f42c4c1328b0e9145ef0746d0c67
ehasanovic4 <ehasanovic4@etf.unsa.ba>
2020-11-04 15:15:32 +0100

    test

---
+++ test/ba/unsa/etf/rpr/KorpaTest.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KorpaTest {
+
+    @org.junit.jupiter.api.Test
+    void dodajArtikl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void izbaciArtiklSaKodom() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dajUkupnuCijenuArtikala() {
+    }
+}
---
+++ test/ba/unsa/etf/rpr/SupermarketTest.java
@@ -0,0 +1,14 @@
+package ba.unsa.etf.rpr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupermarketTest {
+
+    @org.junit.jupiter.api.Test
+    void dodajArtikl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void izbaciArtiklSaKodom() {
+    }
+}
